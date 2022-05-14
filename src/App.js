8942d330cf6ac0f35aca1f7205772c60d3958042
the-yash-rajput <yashrajputishu.gmail.com>
2022-05-14 14:55:20 +0530

    Initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
