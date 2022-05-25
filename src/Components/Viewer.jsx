017e97889966413af2bac9b54101357844436032
the-yash-rajput <yashrajputishu.gmail.com>
2022-05-26 00:50:05 +0530

    worked on home page

--- src/Components/Viewer.jsx
+++ src/Components/Viewer.jsx
@@ -4,11 +4,21 @@ import styled from 'styled-components';
 const Viewer = (props) => {
   return (
     <Container>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
-      <div>1</div>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+      </Wrap>
     </Container>
   )
 };
@@ -25,5 +35,44 @@ const Container = styled.div`
         grid-template-columns: repeat(1, minmax(0, 1fr));
     }
 `;
-
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
+  video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+    video {
+      opacity: 1;
+    }
+  }
+`;
 export default Viewer;
